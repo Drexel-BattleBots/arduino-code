c01fa7b9a5f0877baa9002fadb6e0be13e48ec8c
AbyssArchitect <justyntime2024@gmail.com>
2026-04-28 16:18:48 -0400

    Implement better weapon arm/disarm using ISR; Add check for locomotion to allow throttling

--- arduino-code.ino
+++ arduino-code.ino
@@ -2,18 +2,53 @@
 #include "src/Weapon.h"
 #include "src/Receiver.h"
 
-#define WEAPON_THROTTLE_CHANNEL 3
+#define WEAPON_THROTTLE_CHANNEL 2
+#define LOCOMOTION_THROTTLE_CHANNEL 3
+#define WEAPON_TOGGLE_CHANNEL 6
+#define WEAPON_TOGGLE_PIN 2
+#define LOCOMOTION_TOGGLE_CHANNEL 5
 
 Locomotion locomotion{};
 Weapon weapon{};
 Receiver receiver{};
 
+volatile uint32_t weaponToggleStartTime = 0;
+volatile uint32_t weaponTogglePulseWidth = 1500;
+
+void isrWeaponTogglePulse() {
+	if (digitalRead(WEAPON_TOGGLE_PIN) == HIGH) {
+		weaponToggleStartTime = micros();
+	} else {
+		weaponTogglePulseWidth = micros() - weaponToggleStartTime;
+	}
+}
+
 void setup() {
 	Serial.begin(9600);
 	Serial.println("Serial Monitor Initialized");
+
+	if (digitalPinToInterrupt(WEAPON_TOGGLE_PIN) != -1) { // Check if pin is interrupt-capable
+		Serial.println("ALL GOOD");
+		pinMode(WEAPON_TOGGLE_PIN, INPUT);
+		attachInterrupt(digitalPinToInterrupt(WEAPON_TOGGLE_PIN), isrWeaponTogglePulse, CHANGE);
+	} else {
+		Serial.println("NOT GOOD");
+	}
 }
 
 void loop() {
+	bool armWeapon = (weaponTogglePulseWidth > 1750);
+
+	if (armWeapon && !weapon.isArmed()) {
+		weapon.arm();
+		weapon.enable();
+		Serial.println("Weapon Armed & Enabled");
+	} else if (!armWeapon && weapon.isArmed()) {
+		weapon.disarm();
+		weapon.disable();
+		Serial.println("Weapon Disarmed & Disabled");
+	}
+
 	if (Serial.available()) {
 		// Read incoming data and update control values
 
@@ -49,6 +84,15 @@ void loop() {
 			Serial.println("Unknown Command");
 		}
 	}
+
+		if (locomotion.isEnabled()) {
+			unsigned long locomotion_throttle = receiver.readChannelRaw(LOCOMOTION_THROTTLE_CHANNEL);
+			Serial.print("Locomotion Throttle: ");
+			Serial.println(locomotion_throttle);
+			locomotion.setThrottle(locomotion_throttle);
+		} else {
+			//Serial.println("Weapon is disabled, skipping throttle update");
+		}
 
 	//locomotion.setThrottle(receiver.readChannel(LOCOMOTION_CONTROL_PIN));
 	if (weapon.isEnabled()) {
